72e6d18f55ac85524925692fc6e15b942189b865
Daniel Bravo <bravoappdev@gmail.com>
2017-02-17 12:22:21 -0800

    Created a test function for the Token to_string() function.

--- cpsc323_project_1.cpp
+++ cpsc323_project_1.cpp
@@ -1,9 +1,39 @@
 //
-//  cpsc323_project_1.cpp
-//  
+//  main.cpp
+//  cpsc323_project_1
 //
 //  Created by Daniel Bravo on 2/17/17.
-//
+//  Copyright © 2017 Daniel Bravo. All rights reserved.
 //
 
-#include "cpsc323_project_1.hpp"
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+void testToken(string cntrl, string test);
+
+int main(int argc, const char * argv[]) {
+    string test ="(:token 3 string :str \"ASCII:\")";
+    
+    Token t = *new Token(); // (:token 3 string :str "ASCII:")
+    t.ln = 3;
+    t.id = "string";
+    t.str= "ASCII:";
+    //    t.ix = 4;
+    
+    
+    
+    
+    cout << t.to_string() << endl;
+    testToken("(:token 3 string :str \"ASCII:\")", t.to_string());
+    cout << endl;
+}
+
+void testToken(string cntrl, string test) {
+    if(cntrl.compare(test) == 0)
+        cout << "Success! Token are equal";
+    else
+        cout << "You failed";
+}
+
