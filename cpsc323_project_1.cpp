96bc3d97b3de4542bb444ad30f995140816ea0fb
Daniel Bravo <bravoappdev@gmail.com>
2017-02-17 10:41:46 -0800

    Initial commit

---
+++ cpsc323_project_1.cpp
@@ -0,0 +1,9 @@
+//
+//  cpsc323_project_1.cpp
+//  
+//
+//  Created by Daniel Bravo on 2/17/17.
+//
+//
+
+#include "cpsc323_project_1.hpp"
---
+++ cpsc323_project_1.hpp
@@ -0,0 +1,14 @@
+//
+//  cpsc323_project_1.hpp
+//  
+//
+//  Created by Daniel Bravo on 2/17/17.
+//
+//
+
+#ifndef cpsc323_project_1_hpp
+#define cpsc323_project_1_hpp
+
+#include <stdio.h>
+
+#endif /* cpsc323_project_1_hpp */
